29068b97ba4a17d444d056a59f9a2ff326057f80
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-05-27 20:27:37 +0300

    Added basic websocket support
    Basic echo test for websocket can be found at "/" route

--- src/backend/Titeenipeli/Program.cs
+++ src/backend/Titeenipeli/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -37,6 +38,20 @@ app.MapGet("/weatherforecast", () =>
     .WithOpenApi();
 
 app.MapGet("/ping", () => "pong").WithName("Ping").WithOpenApi();
+
+// <snippet_UseWebSockets>
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
+// </snippet_UseWebSockets>
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
 
 app.Run();
 
