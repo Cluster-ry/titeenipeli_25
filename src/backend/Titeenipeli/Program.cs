60145766f7b486f38c1f4f535de21ee87a75f6b5
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-06-04 23:28:36 +0300

    Added authorization support to Swagger for testing

--- src/backend/Titeenipeli/Program.cs
+++ src/backend/Titeenipeli/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Titeenipeli.Helpers;
 using Titeenipeli.Middleware;
 
@@ -15,7 +16,28 @@ public static class Program
         // Add services to the container.
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Description = "Bearer Authentication with JWT Token",
+                Type = SecuritySchemeType.Http
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme {
+                        Reference = new OpenApiReference {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List < string > ()
+                }
+            });
+        });
         builder.Services.AddControllers();
 
         builder.Services.AddAuthentication(opt =>
