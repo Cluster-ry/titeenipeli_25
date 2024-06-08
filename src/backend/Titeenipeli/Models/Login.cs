e05190539d8f35d90c9acaaffb73517c49772c43
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-06-08 15:25:04 +0300

    Made all fields required in Login object

--- src/backend/Titeenipeli/Models/Login.cs
+++ src/backend/Titeenipeli/Models/Login.cs
@@ -2,6 +2,6 @@ namespace Titeenipeli.Models;
 
 public class Login
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
 }
