623d75650ecb180c3507111fa4e449f137ca1212
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-06-09 20:31:52 +0300

    Project cleanup

--- src/backend/Titeenipeli/Schema/GameEvent.cs
+++ src/backend/Titeenipeli/Schema/GameEvent.cs
@@ -6,6 +6,7 @@ namespace Titeenipeli.Schema;
 public class GameEvent : Entity
 {
     public required User User { get; init; }
+
     [Column(TypeName = "VARCHAR")]
     [StringLength(1024)]
     public required string Event { get; init; }
