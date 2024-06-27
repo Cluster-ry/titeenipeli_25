b3c1835c349bb7d2a00f21ea91d0a4f5210f53f8
softgitron <18495705+softgitron@users.noreply.github.com>
2024-06-27 13:21:02 +0300

    #124: Extract periodic task core to superclass

--- src/backend/Titeenipeli/BackgroundServices/PeriodicPrintToConsoleService.cs
+++ src/backend/Titeenipeli/BackgroundServices/PeriodicPrintToConsoleService.cs
@@ -1,36 +1,18 @@
 namespace Titeenipeli.BackgroundServices;
 
-public class PeriodicPrintToConsoleService(ILogger<PeriodicPrintToConsoleService> logger) : BackgroundService
+public class PeriodicPrintToConsoleService(ILogger<PeriodicPrintToConsoleService> logger) :
+    AsynchronousTimedBackgroundService<PeriodicPrintToConsoleService>(logger)
 {
-    private readonly TimeSpan period = TimeSpan.FromMinutes(1);
-    private readonly ILogger<PeriodicPrintToConsoleService> _logger = logger;
+    protected override TimeSpan Period => TimeSpan.FromMinutes(1);
 
     private int _executionCount;
 
-    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
-    {
-        _logger.LogInformation("Periodic print to console service is running!");
-
-        DoWork();
-
-        using PeriodicTimer timer = new(period);
-        try
-        {
-            while (await timer.WaitForNextTickAsync(cancellationToken))
-            {
-                DoWork();
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            _logger.LogInformation("Periodic print to console service is stopping!");
-        }
-    }
-
-    private void DoWork()
+    protected override Task DoWork()
     {
         _executionCount++;
 
-        _logger.LogInformation("Periodic print to console: #{_executionCount}", _executionCount);
+        Logger.LogInformation("Periodic print to console: #{_executionCount}", _executionCount);
+
+        return Task.CompletedTask;
     }
 }
