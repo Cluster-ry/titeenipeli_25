dfa2368946ee939cc6b17206d9b962cba07c1229
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-07-07 14:52:32 +0300

    #146 Handle possible null reference in JwtHandler

--- src/backend/Titeenipeli/Handlers/JwtHandler.cs
+++ src/backend/Titeenipeli/Handlers/JwtHandler.cs
@@ -29,7 +29,7 @@ public class JwtHandler
                 X = user.SpawnX,
                 Y = user.SpawnY
             },
-            GuildId = user.Guild.Color
+            GuildId = user.Guild?.Color
         };
     }
 
