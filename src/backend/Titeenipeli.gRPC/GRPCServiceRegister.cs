5a057d37d8744ca2a60a4615502f700429f8bc45
softgitron <18495705+softgitron@users.noreply.github.com>
2024-07-16 23:41:40 +0300

    #99: Initialize gRPC server

---
+++ src/backend/Titeenipeli.gRPC/GRPCServiceRegister.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Titeenipeli.gRPC.Services;
+
+namespace Titeenipeli.gRPC;
+
+public static class GRPCServiceRegister
+{
+    public static void AddGRPCServices(WebApplication app)
+    {
+        app.UseGrpcWeb();
+        app.MapGrpcService<IncrementalMapUpdateService>().EnableGrpcWeb();
+
+        IWebHostEnvironment env = app.Environment;
+        if (env.IsDevelopment())
+        {
+            app.MapGrpcReflectionService();
+        }
+    }
+}
---
+++ src/backend/Titeenipeli.gRPC/Services/IncrementalMapUpdateService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using GrpcGeneratedServices;
+using Microsoft.Extensions.Logging;
+
+namespace Titeenipeli.gRPC.Services;
+
+public class IncrementalMapUpdateService(ILogger<IncrementalMapUpdateService> logger) : MapUpdate.MapUpdateBase
+{
+    private readonly ILogger<IncrementalMapUpdateService> _logger = logger;
+
+    public override Task<IncrementalMapUpdateResponse> GetIncremental(IncrementalMapUpdateRequest request, ServerCallContext context)
+    {
+        _logger.LogInformation("IncrementalMapUpdate gRPC endpoint received: {request}", request);
+        return Task.FromResult(new IncrementalMapUpdateResponse());
+    }
+}
