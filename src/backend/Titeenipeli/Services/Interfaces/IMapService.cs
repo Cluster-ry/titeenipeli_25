e4f0562050c0bc631d5c183219ad7ae3ee1707b3
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-07-19 18:08:29 +0300

    #162 Moved pixel placement validation to MapService

--- src/backend/Titeenipeli/Services/Interfaces/IMapService.cs
+++ src/backend/Titeenipeli/Services/Interfaces/IMapService.cs
@@ -1,3 +1,4 @@
+using Titeenipeli.Models;
 using Titeenipeli.Schema;
 
 namespace Titeenipeli.Services.Interfaces;
@@ -5,4 +6,5 @@ namespace Titeenipeli.Services.Interfaces;
 public interface IMapService
 {
     public List<Pixel> GetPixels();
+    public bool IsValidPlacement(Coordinate pixelCoordinate, User user);
 }
--- src/backend/Titeenipeli/Services/MapService.cs
+++ src/backend/Titeenipeli/Services/MapService.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Titeenipeli.Context;
+using Titeenipeli.Models;
 using Titeenipeli.Schema;
 using Titeenipeli.Services.Interfaces;
 
@@ -20,5 +21,18 @@ public class MapService : IMapService
             .Include(pixel => pixel.User)
             .ThenInclude(pixelOwner => pixelOwner!.Guild)
             .OrderBy(pixel => pixel.Y).ToList();
+    }
+
+    public bool IsValidPlacement(Coordinate pixelCoordinate, User user)
+    {
+        // Take neighboring pixels for the pixel the user is trying to set,
+        // but remove cornering pixels and only return pixels belonging to
+        // the user
+        return (from pixel in _dbContext.Map
+                where Math.Abs(pixel.X - pixelCoordinate.X) <= 1 &&
+                      Math.Abs(pixel.Y - pixelCoordinate.Y) <= 1 &&
+                      Math.Abs(pixel.X - pixelCoordinate.X) + Math.Abs(pixel.Y - pixelCoordinate.Y) <= 1 &&
+                      pixel.User == user
+                select pixel).Any();
     }
 }
