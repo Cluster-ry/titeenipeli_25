76b0323eb3e788f7564e1d9deddc0a4e3059481d
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-07-30 21:54:44 +0300

    #132 Calculate the spawn point relative to map center - spawn point should be calculated relative to the map center instead of (0, 0)

--- src/backend/Titeenipeli/Services/SpawnGeneratorService.cs
+++ src/backend/Titeenipeli/Services/SpawnGeneratorService.cs
@@ -8,6 +8,7 @@ namespace Titeenipeli.Services;
 
 public class SpawnGeneratorService
 {
+    private readonly Coordinate _mapCenter;
     private readonly IMapRepositoryService _mapRepositoryService;
     private readonly SpawnGenerator _spawnGenerator;
 
@@ -15,17 +16,27 @@ public class SpawnGeneratorService
     {
         _mapRepositoryService = mapRepositoryService;
         _spawnGenerator = new SpawnGenerator(gameOptions);
+        _mapCenter = new Coordinate { X = gameOptions.Width / 2, Y = gameOptions.Height / 2 };
     }
 
     public Coordinate GetSpawnPoint(GuildName guildName)
     {
-        Coordinate spawnCoordinate = _spawnGenerator.GetSpawnPoint(guildName);
+        Coordinate spawnCoordinate = AddOffset(_spawnGenerator.GetSpawnPoint(guildName));
 
         while (_mapRepositoryService.IsSpawn(spawnCoordinate))
         {
-            spawnCoordinate = _spawnGenerator.GetSpawnPoint(guildName);
+            spawnCoordinate = AddOffset(_spawnGenerator.GetSpawnPoint(guildName));
         }
 
         return spawnCoordinate;
+    }
+
+    private Coordinate AddOffset(Coordinate coordinate)
+    {
+        return new Coordinate
+        {
+            X = _mapCenter.X + coordinate.X,
+            Y = _mapCenter.Y + coordinate.Y
+        };
     }
 }
