5f6ce3636589d867b7e17eb7fda3dce0e08c20be
CaptainCluster <sranta.ville@gmail.com>
2024-10-24 11:28:21 +0300

    241 improved documentation

--- src/frontend/src/stores/store.tsx
+++ src/frontend/src/stores/store.tsx
@@ -145,9 +145,6 @@ export const useGameMapStore = create<GameMap>((set, get) => ({
     }
   },
 
-  /**
-   *
-   */
   doIncrementalUpdate: (
     incrementalUpdateResponse: IncrementalMapUpdateResponse
   ) => {
