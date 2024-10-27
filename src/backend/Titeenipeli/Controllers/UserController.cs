7f0e4ea2fb129b84a309cd75780839a15c74377f
KeManen <dev@kema.fi>
2024-10-27 15:38:54 +0200

    Removed more merge fuckery

--- src/backend/Titeenipeli/Controllers/UserController.cs
+++ src/backend/Titeenipeli/Controllers/UserController.cs
@@ -50,13 +50,7 @@ public class UserController(
     }
 
     public class UserResult
-    public class UserResult
     {
-        public int Id { get; init; }
-        public string Username { get; init; }
-        public string FirstName { get; init; }
-        public string LastName { get; init; }
-        public int Guild { get; init; }
         public int Id { get; init; }
         public string Username { get; init; }
         public string FirstName { get; init; }
