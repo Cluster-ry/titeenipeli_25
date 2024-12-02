d8797080f3a3b3389545cadb93f0ce3d34a603d3
CaptainCluster <sranta.ville@gmail.com>
2024-12-02 17:11:50 +0200

    Input field for CTF token

--- src/frontend/src/components/Ctf.tsx
+++ src/frontend/src/components/Ctf.tsx
@@ -1,20 +1,38 @@
+import { useState } from 'react';
 import { postCtf } from "../api/ctf";
 import { PostCtfInput } from "../models/Post/PostCtfInput";
-import "../pages/Game/Overlay/overlay.css"; 
+import "../pages/Game/Overlay/overlay.css";
 
 const Ctf = () => {
-  const TEST_DISCLAIMER = "[TEST] CTF";
- 
-  // NOTE! IMPLEMENTATION COULD BE WORTH CHANGING IN THE FUTURE!
-  const ctfToken: PostCtfInput = {
-    token: "#TEST_FLAG"
-  } 
-  
+  const [disclaimer, setDisclaimer] = useState("[TEST] CTF");
+  const [token, setToken] = useState("");
+
+  const handleTokenChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setToken(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const ctfToken: PostCtfInput = {
+      token: token
+    };
+    postCtf(ctfToken);
+  };
+
   return (
-    <div className="ctf-icon" onClick={() => postCtf(ctfToken)}>
-      {TEST_DISCLAIMER}
+    <div className="ctf-container">
+      <input
+        type="text"
+        placeholder="Enter Token"
+        value={token}
+        onChange={handleTokenChange}
+        style={{pointerEvents: "all", bottom: "0"}}
+      />
+      <div className="ctf-icon" onClick={handleSubmit}>
+        {disclaimer}
+      </div>
     </div>
   );
 }
 
 export default Ctf;
+
