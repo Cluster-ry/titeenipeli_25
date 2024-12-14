c2c7e0fc197ced013deb162b782a6b38a2cafa4e
CaptainCluster <sranta.ville@gmail.com>
2024-12-15 00:16:08 +0200

    Neutral type for the notifications

--- src/frontend/src/components/Notification.tsx
+++ src/frontend/src/components/Notification.tsx
@@ -1,10 +1,17 @@
 import "./notification.css"
 const Notification = ({ type, text }: { type: string; text: string }) => {
-  return (
-    <div className={`notification ${type}`}>
-      {text}
-    </div>
-  )
+  
+  if (type.length === 0) {
+    type = "neutral"
+  }
+
+  if (text.length > 0) {
+    return (
+      <div className={`notification ${type}`}>
+        {text}
+      </div>
+    )
+  } 
 }
 
 export default Notification;
