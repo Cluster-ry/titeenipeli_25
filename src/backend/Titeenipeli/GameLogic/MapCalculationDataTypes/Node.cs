b162246c9d5362086400879db1513d3cbfcaf257
softgitron <18495705+softgitron@users.noreply.github.com>
2025-01-01 17:27:28 +0200

    #307: Fill fills only not owned pixels

--- src/backend/Titeenipeli/GameLogic/MapCalculationDataTypes/Node.cs
+++ src/backend/Titeenipeli/GameLogic/MapCalculationDataTypes/Node.cs
@@ -5,7 +5,7 @@ namespace Titeenipeli.GameLogic.MapCalculationDataTypes;
 
 public record Node
 {
-    public required GuildName? Guild { get; init; }
+    public required GuildName? Guild { get; set; }
     public HashSet<int> Neighbours { get; } = [];
     public HashSet<Coordinate> Pixels { get; } = [];
     public bool HasSpawn { get; set; }
