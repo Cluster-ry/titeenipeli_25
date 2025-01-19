03a04154ca7b1353fc786139e210a32e0e2d34c6
softgitron <18495705+softgitron@users.noreply.github.com>
2025-01-19 20:50:52 +0200

    #420: Quick fixes to make mobile look more decent

--- src/frontend/src/pages/Game/Overlay/BottomOverlay.tsx
+++ src/frontend/src/pages/Game/Overlay/BottomOverlay.tsx
@@ -4,19 +4,24 @@ import "./overlay.css";
 import { SpecialEffect } from "./specialEffect";
 
 const BottomOverlay = () => {
-    const [selected, setSelected] = useState<number|null>(null);
+    const [selected, setSelected] = useState<number | null>(null);
     // Placeholders
     const specialEffects: PowerUp[] = [
-        {Id: 1, Name: "Titeenikirves", Description: "Jeejee", Directed: true},
-        {Id: 2, Name: "Pyssy saatana", Description: "Juujuu", Directed: false},
-        {Id: 3, Name: "Taktinen anustappi", Description: "Jiijee", Directed: false},
+        { Id: 1, Name: "Titeeni-\nkirves", Description: "Jeejee", Directed: true },
+        { Id: 2, Name: "Pyssy saatana", Description: "Juujuu", Directed: false },
+        { Id: 3, Name: "Taktinen anustappi", Description: "Jiijee", Directed: false },
     ];
-    const onClick = useCallback((id: number) => {
-        setSelected(prev => prev !== id ? id : null);
-    }, [setSelected])
+    const onClick = useCallback(
+        (id: number) => {
+            setSelected((prev) => (prev !== id ? id : null));
+        },
+        [setSelected],
+    );
     return (
         <div className="bottom-overlay bottom-gradient">
-            {specialEffects.map(effect => <SpecialEffect key={effect.Id} {...effect} selected={selected} onClick={onClick} />)}
+            {specialEffects.map((effect) => (
+                <SpecialEffect key={effect.Id} {...effect} selected={selected} onClick={onClick} />
+            ))}
         </div>
     );
 };
