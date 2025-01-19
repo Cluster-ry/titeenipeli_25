bafc40a3cb9a39241f1c22a6d8e3a969adbdda1d
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2025-01-19 16:30:09 +0200

    #407 Added a way to get cached user information from JWT

--- src/backend/Titeenipeli.Common/Models/JwtClaim.cs
+++ src/backend/Titeenipeli.Common/Models/JwtClaim.cs
@@ -5,8 +5,6 @@ namespace Titeenipeli.Common.Models;
 public class JwtClaim
 {
     public required int Id { get; init; }
-
     public required Coordinate CoordinateOffset { get; init; }
-
-    public required GuildName? GuildId { get; init; }
+    public required GuildName Guild { get; init; }
 }
