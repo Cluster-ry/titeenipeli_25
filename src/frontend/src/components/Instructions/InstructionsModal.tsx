b1d9ea48537709877a6a8378ce6daacc3ec6705f
CaptainCluster <sranta.ville@gmail.com>
2025-02-24 12:21:39 +0200

    More instructions. Styling for new instruction sections

--- src/frontend/src/components/Instructions/InstructionsModal.tsx
+++ src/frontend/src/components/Instructions/InstructionsModal.tsx
@@ -9,9 +9,16 @@ const instructionsData: Instructions[] = [
     header: "May the best guild win!",
     instructions: [
       "Help your guild win by clicking and conquering pixels!",
+      "Ticking scores are calculated based on conquered pixels.",
       "Devastate your rivals by severing their pixels from their spawn points!",
       "Keep an eye on your bucket points! Your guild needs momentum!",
-      "Manipulate destiny with game-changing special effects!"
+    ]
+  },
+  {
+    header: "Power-ups",
+    instructions: [
+      "Manipulate destiny with game-changing special effects!",
+      "Receive power-ups via CTF tokens."
     ]
   }
 ];
