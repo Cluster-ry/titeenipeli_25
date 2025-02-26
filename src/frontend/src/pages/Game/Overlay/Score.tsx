42468930bb084ce0f4436eedd8207b43916d2fa1
CaptainCluster <sranta.ville@gmail.com>
2025-02-26 13:22:20 +0200

    Medals visible

--- src/frontend/src/pages/Game/Overlay/Score.tsx
+++ src/frontend/src/pages/Game/Overlay/Score.tsx
@@ -1,14 +1,45 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import "./overlay.css";
+
+import goldMedal from "../../../assets/sprites/medal-gold.png"
+import silverMedal from "../../../assets/sprites/medal-silver.png"
+import bronzeMedal from "../../../assets/sprites/medal-bronze.png"
+
+
 
 type ScoreProps = {
     guild: string;
     score: number;
+    place: number;
 };
 
-const Score: FC<ScoreProps> = ({ guild, score }) => {
-    return (
+
+const Score: FC<ScoreProps> = ({ guild, score, place }) => {
+  const [displayMedal, setDisplayMedal] = useState<string>("");
+
+  useEffect(() => {
+    switch(place) {
+      case 0:
+        setDisplayMedal(goldMedal);
+        break;
+      case 1:
+        setDisplayMedal(silverMedal);
+        break;
+      case 2:
+        setDisplayMedal(bronzeMedal);
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  return (
         <div key={guild} className="top-overlay__score-container">
+            {
+              displayMedal.length > 0 
+                ? <img src={displayMedal} />
+                : <></>
+            }
             <span>{guild}:</span>
             <span>{score}</span>
         </div>
