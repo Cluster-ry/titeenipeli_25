54ca015650d7d346026f66de233f33cf34599fec
G3neraattori <71793648+G3neraattori@users.noreply.github.com>
2025-03-03 00:05:14 +0200

    more effects

---
+++ src/backend/Titeenipeli.SpecialEffects/GlitchEffect.cs
@@ -0,0 +1,27 @@
+﻿using Titeenipeli.Common.Models;
+
+namespace Titeenipeli.SpecialEffects;
+
+public class GlitchEffect : BaseSpecialEffect
+{
+    public override string Description { get; } = "";   //TODO
+                                                //jotain glitchi tekstiä
+    protected override byte[,] Template { get; } = GetGlitchTemplate();
+
+    private static byte[,] GetGlitchTemplate()
+    {
+        byte[,] bytes = new byte[5,5];
+        Random rnd = new Random();
+        
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                bytes[i, j] = (byte)rnd.Next(0, 2);
+            }
+        }
+        return bytes;
+    }
+
+    protected override Coordinate Origin { get; } = new(0, 0);
+}
---
+++ src/backend/Titeenipeli.SpecialEffects/ShapeEffects/BinaryEffect.cs
@@ -0,0 +1,32 @@
+using Titeenipeli.Common.Enums;
+using Titeenipeli.Common.Models;
+
+namespace Titeenipeli.SpecialEffects;
+
+public class BinaryEffect : BaseSpecialEffect
+{
+    public override string Description { get; } = "01010100 01001001 01010100 01000101 01000101 01001110 01001001 01010100";
+    
+    protected override byte[,] Template { get; } =
+    {
+        {1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1},
+        {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+        {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+        {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+        {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1},
+        {1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+        {1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+        {1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+        {1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+        {1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1}
+    };
+    
+    public override List<Coordinate> HandleSpecialEffect(Coordinate location, Direction direction)
+    {
+        return base.HandleSpecialEffect(location, Direction.East);
+    }
+    protected override Coordinate Origin { get; } = new(5, 5);
+}
