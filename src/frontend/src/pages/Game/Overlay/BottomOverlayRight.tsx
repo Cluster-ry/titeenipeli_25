905dcca31e6e5238fc98513660e3ed6b35ca3079
CaptainCluster <sranta.ville@gmail.com>
2025-03-04 13:32:30 +0200

    Hamburger menu now in help icon

--- src/frontend/src/pages/Game/Overlay/BottomOverlayRight.tsx
+++ src/frontend/src/pages/Game/Overlay/BottomOverlayRight.tsx
@@ -1,9 +1,14 @@
 import infoIcon from "../../../assets/new_assets/info.png"
 import "./overlay.css"
+import { useCtfStore } from "../../../stores/ctfModalStore"
 
 const BottomOverlayRight = () => {
+  const { setCtfModelOpenState } = useCtfStore();
   return (
-    <div className="bottom-overlay__right">
+    <div className="bottom-overlay__right" 
+      onClick={() => {
+        setCtfModelOpenState(true)
+      }}>
       <img className="bottom-overlay__right__help" src={infoIcon} />
     </div>
   );
--- src/frontend/src/pages/Game/Overlay/TopOverlayRight.tsx
+++ src/frontend/src/pages/Game/Overlay/TopOverlayRight.tsx
@@ -1,11 +1,9 @@
 import { FC, PropsWithChildren } from "react";
 import "./overlay.css";
-import HamburgerMenuButton from "../../../components/Ctf/HamburgerMenuButton";
 
 const TopOverlayRight: FC<PropsWithChildren> = () => {
     return (
         <div className="top-overlay__right">
-            <HamburgerMenuButton />
         </div>
     );
 };
