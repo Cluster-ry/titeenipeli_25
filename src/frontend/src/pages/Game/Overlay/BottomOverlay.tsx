53cdd1e69f58de95c3abaa0a8fab57fdcc7aa9c5
CaptainCluster <sranta.ville@gmail.com>
2025-03-06 17:36:23 +0200

    Made powerup button work

--- src/frontend/src/pages/Game/Overlay/BottomOverlay.tsx
+++ src/frontend/src/pages/Game/Overlay/BottomOverlay.tsx
@@ -2,30 +2,47 @@ import "./overlay.css";
 import { SpecialEffect } from "./specialEffect";
 import { useGameStateStore } from "../../../stores/gameStateStore";
 import { usePowerUpStore } from "../../../stores/powerupStore";
+import powerupIcon from "../../../assets/sprites/powerup.png";
 
 import BottomOverlayRight from "./BottomOverlayRight";
-import BottomOverlayMiddle from "./BottomOverlayMiddle";
+import { useState } from "react";
 
 const BottomOverlay = () => {
     const specialEffects = useGameStateStore((state) => state.powerUps);
     const powerUp = usePowerUpStore((state) => state.powerUp);
     const setPowerUp = usePowerUpStore((state) => state.setPowerUp);
+  const [powerupVisible, setPowerupVisible] = useState(false);
 
-    return (
-        <div className="bottom-overlay bottom-gradient">
-            {specialEffects.map((effect, index) => (
-                <SpecialEffect
-                    key={`${effect.powerUpId}-${index}`}
-                    {...effect}
-                    selected={powerUp}
-                    onClick={setPowerUp}
-                />
-            ))}
-            <div className="bottom-overlay__left"></div>
-            <BottomOverlayMiddle />
-            <BottomOverlayRight />
+  return (
+    <div className="bottom-overlay bottom-gradient">
+
+      <div className="bottom-overlay__left">
+      {
+        powerupVisible 
+        ? 
+        specialEffects.map((effect, index) => (
+        <SpecialEffect
+          key={`${effect.powerUpId}-${index}`}
+          {...effect}
+          selected={powerUp}
+          onClick={setPowerUp}
+        />
+        ))
+        : <></>
+      
+      }
+
+      </div>
+      <div className="bottom-overlay__middle">
+        <div
+          className="bottom-overlay__imgwrapper"
+          onClick={() => {setPowerupVisible(!powerupVisible)}}>
+        <img className="bottom-overlay__middle__powerup" src={powerupIcon} />
         </div>
-    );
+      </div>
+      <BottomOverlayRight />
+    </div>
+  );
 };
 
 export { BottomOverlay };
