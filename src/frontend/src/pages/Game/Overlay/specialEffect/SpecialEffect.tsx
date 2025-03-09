98331fd90737005c9e56c960d9d851042ce00d73
G3neraattori <71793648+G3neraattori@users.noreply.github.com>
2025-03-10 00:48:56 +0200

    more icons

--- src/frontend/src/pages/Game/Overlay/specialEffect/SpecialEffect.tsx
+++ src/frontend/src/pages/Game/Overlay/specialEffect/SpecialEffect.tsx
@@ -2,13 +2,18 @@ import { FC, useCallback, useMemo } from "react";
 import "./style.css";
 import defaultEffect from "../../../../assets/special_effect.png";
 import { PowerUp } from "../../../../models/Get/GetGameState";
-//TODO nää vois olla massa import ja filenamella variable
 import titeeniKirvesEffect from "../../../../assets/powerups/powerup-axe.png";
 import ruusuEffect2 from "../../../../assets/powerups/powerup-ruusu-2.png";
 import isoLEffect from "../../../../assets/powerups/powerup-L.png";
 import mFilesEffect from "../../../../assets/powerups/powerup-mfiles.png";
 import siikaEffect from "../../../../assets/powerups/powerup-siika.png";
 import binaryEffect from "../../../../assets/powerups/powerup-binary.png";
+import starEffect from "../../../../assets/powerups/powerup-star.png";
+//import bottleEffect from "../../../../assets/powerups/powerup-bottle.png";
+import dinoEffect from "../../../../assets/powerups/powerup-dino.png";
+import gemEffect from "../../../../assets/powerups/powerup-diamond.png";
+import heartEffect from "../../../../assets/powerups/powerup-heart.png";
+import spaceInvaderEffect from "../../../../assets/powerups/powerup-invader.png";
 
 type Props = {
     selected: number | null;
@@ -18,13 +23,18 @@ type Props = {
 const cancelLabel = "Cancel";
 
 const SpecialEffectIcon: { [key: number]: string } = {
-    1: defaultEffect, 
+    1: defaultEffect,
     0: titeeniKirvesEffect,
-    2: mFilesEffect, 
-    3: isoLEffect, 
-    4: ruusuEffect2, 
+    2: mFilesEffect,
+    3: isoLEffect,
+    4: ruusuEffect2,
     5: binaryEffect,
     6: siikaEffect,
+    8: starEffect,
+    10: dinoEffect,
+    11: gemEffect,
+    12: heartEffect,
+    13: spaceInvaderEffect
 };
 
 const getEffectIcon = (index: number) => {
