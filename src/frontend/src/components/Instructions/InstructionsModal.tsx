7f59cc23e8d5ecad4ef90d854e38ac9b9e12af05
softgitron <18495705+softgitron@users.noreply.github.com>
2025-03-12 23:39:03 +0200

    #554: Fix instruction images

--- src/frontend/src/components/Instructions/InstructionsModal.tsx
+++ src/frontend/src/components/Instructions/InstructionsModal.tsx
@@ -5,12 +5,15 @@ import { Instructions } from "../../models/ComponentData/Instructions";
 import InstructionsEntry from "./InstructionsEntry";
 import { FC, useCallback } from "react";
 import mFilesLogo from "../../assets/m_files.svg";
+import instructions1 from "../../assets/instructions/instructions_1.webp";
+import instructions2 from "../../assets/instructions/instructions_2.webp";
+
 const instructionsData: Instructions[] = [
     {
         header: "May the best guild win!",
         instructions: [
-            {text: "Help your guild win by clicking and conquering adjacent pixels", img: "src/assets/instructions/instructions_1.webp"},
-            {text: "Conquer swathes of land by encircling areas", img: "src/assets/instructions/instructions_2.webp"},
+            {text: "Help your guild win by clicking and conquering adjacent pixels", img: instructions1},
+            {text: "Conquer swathes of land by encircling areas", img: instructions2},
             {text: "Devastate your rivals by severing their pixels from their spawn points"},
             {text: "Keep an eye on your bucket points! Your guild needs momentum!"},
             {text: "Ticking scores are calculated based on conquered pixels"},
